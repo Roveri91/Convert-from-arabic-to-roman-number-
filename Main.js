f2c7499df655c161ce6c0c0714b2b92c067b1243
unknown <spikeroveri91@gmail.com>
2022-09-03 14:39:23 +0900

    first commit

---
+++ Main.js
@@ -0,0 +1,74 @@
+const numbersAndNumerals = [
+    {
+        number: 1000,
+        roman: `M`
+    },
+    {
+        number: 900,
+        roman: `CM`
+    },
+    {
+        number: 500,
+        roman: `D`
+    },
+    {
+        number: 400,
+        roman: `CD`
+    },
+    {
+        number: 100,
+        roman: `C`
+    },
+    {
+        number: 90,
+        roman: `XC`
+    },
+    {
+        number: 50,
+        roman: `L`
+    },
+    {
+        number: 40,
+        roman: `XL`
+    },
+    {
+        number: 10,
+        roman: `X`
+    },
+    {
+        number: 9,
+        roman: `IX`
+    },
+    {
+        number: 5,
+        roman: `V`
+    },
+    {
+        number: 4,
+        roman: `IV`
+    },
+    {
+        number: 1,
+        roman: `I`
+    }
+];
+
+function convert(arabicNumber) {
+    let romanLetter = '';
+    let number = arabicNumber;
+    
+    for (let i=0; i < numbersAndNumerals.length; i++) {
+        if (numbersAndNumerals[i].number <= number) {
+            number = number - numbersAndNumerals[i].number;
+            romanLetter = romanLetter + numbersAndNumerals[i].roman;
+            i--;
+        }         
+    }
+    document.getElementById(`romanInput`).value = romanLetter;
+}
+
+document.getElementById('convert').addEventListener('click', function(){
+    let number = document.getElementById('numberInput').value;
+    
+    convert(number);
+});
